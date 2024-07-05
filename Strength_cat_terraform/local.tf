1ce47a3c17881a308575f60e62d2c6ec485967dc
VishnuSharma11D00 <155800287+VishnuSharma11D00@users.noreply.github.com>
2024-07-05 21:00:24 +0530

    Update local.tf

--- Strength_cat_terraform/local.tf
+++ Strength_cat_terraform/local.tf
@@ -1,40 +1,40 @@
 locals {
   #dynamo db variables
-  dynamodb_table_name = "strength_cat_dynamodb"
+  dynamodb_table_name = "strength_cat_dynamodb"                                   # name of your dynamodb
   dynamo_db_table_hash_key = "username"
   dynamodb_table_range_key = "timestamp"
 
 
   #lambda variables
-  lambda_role_name = "lambda_execution_role"
+  lambda_role_name = "lambda_execution_role"   #name of your lambda execution role
 
-  lambda_function_name = "strength_cat_lambda"
+  lambda_function_name = "strength_cat_lambda"    #name of your lambda function
   lambda_runtime = "python3.12"
-  lambda_handler = "strenCat_lambda_fun_percentage.lambda_handler" #the lambda-file-name.lambda_handler"
-  python_code_path = "modules/python_code/"
-  output_zip_name = "modules/python_code/lambda.zip"
-  python_zip_path = "modules/python_code/lambda.zip"
+  lambda_handler = "strenCat_lambda_fun_percentage.lambda_handler"                  # name of lambda-file-name.lambda_handler"
+  python_code_path = "modules/python_code/"                                         # location of the python code
+  output_zip_name = "modules/python_code/lambda.zip"                                # name your zip file for the function that will be created
+  python_zip_path = "modules/python_code/lambda.zip"                                # should be same as above
 
 
   #lambda history fuction variables
-  lambda_history_iam_role_name = "lambda_history_execution_role"
+  lambda_history_iam_role_name = "lambda_history_execution_role"                     # name of your lambda history function execution role
 
-  lambda_history_function_name = "strength_cat_lambda_history"
-  lambda_history_function_runtime = "python3.12"
-  lambda_history_function_handler = "history_lambda_code.lambda_handler" #the lambda-file-name.lambda_handler"
-  python_historycode_path = "modules/python_code_history/"
-  python_historycode_zip_path = "modules/python_code_history/lambda_history.zip"
-  python_historycode_zip_output_path = "modules/python_code_history/lambda_history.zip"
-
+  lambda_history_function_name = "strength_cat_lambda_history"                       # name of your lambda history function 
+  lambda_history_function_runtime = "python3.12"                                     
+  lambda_history_function_handler = "history_lambda_code.lambda_handler"             #the lambda-history-file-name.lambda_handler"
+  python_historycode_path = "modules/python_code_history/"                           # location of the python code
+  python_historycode_zip_path = "modules/python_code_history/lambda_history.zip"     # name your zip file for the history function that will be created
+  python_historycode_zip_output_path = "modules/python_code_history/lambda_history.zip"  # should be same as above
+ 
 
   # api-gateway variables
-  api_gateway_name = "strength_cat_apigateway"
-  api_gateway_stage_name = "dev"
-  region = "us-east-1"
+  api_gateway_name = "strength_cat_apigateway"                                       # api gateway name
+  api_gateway_stage_name = "dev"                                                     # api gateway stage name
+  region = "us-east-1"                                                               # Region
 
 
   # Amplify variables
-  git_repo = "https://github.com/VishnuSharma11D00/Strength_categorization.git"
-  github_access_token = "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"
-  app_name = "strength_cat"
+  git_repo = "----------------------------------------------------------------"      # your git repo
+  github_access_token = "-----------------------------------------------------"      # your git hub access token
+  app_name = "strength_cat"                                                          # name of your app
 }
